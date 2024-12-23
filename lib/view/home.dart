20f9145a9ced52fbc9a957dac8fd106342a6d8d3
Saifeddine Kasmi <skasmi@e2r2p6.1337.ma>
2024-12-24 00:36:26 +0100

    update and add a effect when i click in ligand

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -3,6 +3,226 @@ import 'package:flutter/services.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:swifty_protein/view/model_viewer.dart';
 
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({super.key});
+
+//   @override
+//   State<HomeScreen> createState() => _HomeScreenState();
+// }
+
+// class _HomeScreenState extends State<HomeScreen> {
+//   List<List<String>> _ligands = [];
+//   bool isLoading = true;
+//   String _selectedLigand = '';
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _loadData().then(_parseData);
+//   }
+
+//   Future<String> _loadData() async {
+//     return await rootBundle.loadString('assets/ligands.txt');
+//   }
+
+//   Future<void> _parseData(String content) async {
+//     List<String> lines = content.split('\n');
+//     List<List<String>> tempLigands = [];
+
+//     for (var line in lines) {
+//       if (line.trim().isNotEmpty) {
+//         List<String> parts = line.split(',');
+//         tempLigands.add(parts);
+//       }
+//     }
+
+//     setState(() {
+//       _ligands = tempLigands;
+//       isLoading = false;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: Center(
+//         child: SafeArea(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             children: [
+//               if (isLoading)
+//                 const CircularProgressIndicator()
+//               else
+//                 Padding(
+//                   padding: const EdgeInsets.all(16.0),
+//                   child: SearchAnchor(
+//                     viewBackgroundColor: Colors.white,
+//                     builder:
+//                         (BuildContext context, SearchController controller) {
+//                       return SearchBar(
+//                         controller: controller,
+//                         padding: const WidgetStatePropertyAll<EdgeInsets>(
+//                           EdgeInsets.symmetric(horizontal: 16.0),
+//                         ),
+//                         backgroundColor: WidgetStatePropertyAll(
+//                           Colors.grey[200],
+//                         ),
+//                         shadowColor: const WidgetStatePropertyAll(
+//                           Colors.transparent,
+//                         ),
+//                         onTap: () {
+//                           controller.openView();
+//                         },
+//                         leading: const Icon(Icons.search),
+//                         hintText: 'Search for ligands',
+//                         hintStyle: WidgetStateProperty.all<TextStyle>(
+//                           const TextStyle(
+//                             fontSize: 18.0,
+//                             fontWeight: FontWeight.bold,
+//                             fontFamily: '_2',
+//                           ),
+//                         ),
+//                       );
+//                     },
+//                     suggestionsBuilder:
+//                         (BuildContext context, SearchController controller) {
+//                       final keyword = controller.text.toLowerCase();
+//                       return _ligands
+//                           .where((ligand) => ligand.any((element) =>
+//                               element.toLowerCase().contains(keyword)))
+//                           .map((ligand) => ListTile(
+//                                 title: Text(ligand[0],
+//                                     style: const TextStyle(
+//                                         fontFamily: '_2',
+//                                         fontWeight: FontWeight.bold)),
+//                                 subtitle: Text(
+//                                     ligand.length > 1 ? ligand[1] : '',
+//                                     style: const TextStyle(fontFamily: '_2')),
+//                                 onTap: () {
+//                                   controller.closeView(ligand[0]);
+//                                   setState(() {
+//                                     _selectedLigand = ligand[0];
+//                                   });
+//                                 },
+//                               ))
+//                           .toList();
+//                     },
+//                   ),
+//                 ),
+//               const SizedBox(height: 20),
+//               if (_selectedLigand.isNotEmpty)
+//                 Text(
+//                   'Selected ligand: $_selectedLigand',
+//                   style: const TextStyle(
+//                       fontSize: 20,
+//                       color: Colors.blue,
+//                       fontWeight: FontWeight.bold,
+//                       fontFamily: '_2'),
+//                 )
+//               else
+//                 const SizedBox(),
+//               const SizedBox(height: 20),
+//               if (_selectedLigand.isNotEmpty)
+//                 SizedBox(
+//                   width: 200,
+//                   height: 50,
+//                   child: ElevatedButton(
+//                     style: ElevatedButton.styleFrom(
+//                       backgroundColor: Colors.blue,
+//                       foregroundColor: Colors.white,
+//                       textStyle: const TextStyle(fontSize: 20),
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(10.0),
+//                       ),
+//                     ),
+//                     onPressed: () {
+//                       Navigator.push(
+//                         context,
+//                         MaterialPageRoute(
+//                           builder: (context) =>
+//                               MoleculeScreen(id: _selectedLigand),
+//                         ),
+//                       ).then((value) {
+//                         setState(() {
+//                           _selectedLigand = '';
+//                         });
+//                       });
+//                     },
+//                     child: const Text(
+//                       'View Model',
+//                       style: TextStyle(
+//                           fontFamily: '_2', fontWeight: FontWeight.bold),
+//                     ),
+//                   ),
+//                 )
+//               else
+//                 const SizedBox(),
+//               const SizedBox(height: 20),
+//               Expanded(
+//                 child: GridView.builder(
+//                   padding: const EdgeInsets.all(8.0),
+//                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//                     crossAxisCount: 3,
+//                     childAspectRatio: 1.0,
+//                   ),
+//                   itemCount: _ligands.length,
+//                   itemBuilder: (context, index) {
+//                     return InkWell(
+//                       onTap: () {
+//                         setState(() {
+//                           _selectedLigand = _ligands[index][0];
+//                         });
+//                       },
+
+//                       child: Padding(
+//                         padding: const EdgeInsets.all(3.0),
+//                         child: Container(
+//                           decoration: BoxDecoration(
+//                             borderRadius: BorderRadius.circular(12),
+//                             color: Colors.white,
+//                             boxShadow: [
+//                               BoxShadow(
+//                                 color: Colors.grey.withOpacity(0.5),
+//                                 blurRadius: 7,
+//                                 offset: const Offset(0, 3),
+//                               ),
+//                             ],
+//                           ),
+//                           child: Column(
+//                             mainAxisAlignment: MainAxisAlignment.center,
+//                             children: [
+//                               Padding(
+//                                 padding: const EdgeInsets.all(8.0),
+//                                 child: Image.asset(
+//                                   'assets/images/atom2.png',
+//                                   height: 70,
+//                                 ),
+//                               ),
+//                               const Spacer(),
+//                               Text(
+//                                 _ligands[index][0],
+//                                 style: const TextStyle(
+//                                   fontFamily: '_2',
+//                                   fontWeight: FontWeight.bold,
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                         ),
+//                       ),
+//                     );
+//                   },
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -14,6 +234,7 @@ class _HomeScreenState extends State<HomeScreen> {
   List<List<String>> _ligands = [];
   bool isLoading = true;
   String _selectedLigand = '';
+  final Set<int> _expandedCards = {};
 
   @override
   void initState() {
@@ -110,7 +331,6 @@ class _HomeScreenState extends State<HomeScreen> {
                     },
                   ),
                 ),
-              const SizedBox(height: 20),
               if (_selectedLigand.isNotEmpty)
                 Text(
                   'Selected ligand: $_selectedLigand',
@@ -122,43 +342,6 @@ class _HomeScreenState extends State<HomeScreen> {
                 )
               else
                 const SizedBox(),
-              const SizedBox(height: 20),
-              if (_selectedLigand.isNotEmpty)
-                SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      foregroundColor: Colors.white,
-                      textStyle: const TextStyle(fontSize: 20),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              MoleculeScreen(id: _selectedLigand),
-                        ),
-                      ).then((value) {
-                        setState(() {
-                          _selectedLigand = '';
-                        });
-                      });
-                    },
-                    child: const Text(
-                      'View Model',
-                      style: TextStyle(
-                          fontFamily: '_2', fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                )
-              else
-                const SizedBox(),
-              const SizedBox(height: 20),
               Expanded(
                 child: GridView.builder(
                   padding: const EdgeInsets.all(8.0),
@@ -168,41 +351,19 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   itemCount: _ligands.length,
                   itemBuilder: (context, index) {
+                    bool isExpanded = _expandedCards.contains(index);
+
                     return InkWell(
                       onTap: () {
                         setState(() {
+                          _expandedCards.clear(); // Clear all expanded cards
+                          if (!isExpanded) {
+                            _expandedCards
+                                .add(index); // Add only the newly selected card
+                          }
                           _selectedLigand = _ligands[index][0];
                         });
                       },
-                      // child: Container(
-                      //   decoration: BoxDecoration(
-                      //     borderRadius: BorderRadius.circular(12),
-                      //     color: Colors.white,
-                      //     boxShadow: [
-                      //       BoxShadow(
-                      //         color: Colors.grey.withOpacity(0.5),
-                      //         spreadRadius: 5,
-                      //         blurRadius: 7,
-                      //         offset: Offset(0, 3),
-                      //       ),
-                      //     ],
-                      //   ),
-                      //   child: Column(
-                      //     children: [
-                      //       Icon(Icons.person,
-                      //           size: 24, color: Colors.blueAccent),
-                      //       Container(
-                      //         decoration: const BoxDecoration(
-                      //             color: Colors.blueAccent,
-                      //             borderRadius: BorderRadius.only(
-                      //                 bottomRight: Radius.circular(12),
-                      //                 bottomLeft: Radius.circular(12))),
-                      //         child: Text("Student"),
-                      //         padding: const EdgeInsets.all(12),
-                      //       )
-                      //     ],
-                      //   ),
-                      // ),
                       child: Padding(
                         padding: const EdgeInsets.all(3.0),
                         child: Container(
@@ -217,22 +378,68 @@ class _HomeScreenState extends State<HomeScreen> {
                               ),
                             ],
                           ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                          clipBehavior: Clip.hardEdge,
+                          child: Stack(
                             children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Image.asset(
-                                  'assets/images/atom2.png',
-                                  height: 70,
+                              Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Image.asset(
+                                        'assets/images/atom3.png',
+                                        height: 70,
+                                      ),
+                                    ),
+                                    const Spacer(),
+                                    Text(
+                                      _ligands[index][0],
+                                      style: const TextStyle(
+                                        fontFamily: '_2',
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 8),
+                                  ],
                                 ),
                               ),
-                              const Spacer(),
-                              Text(
-                                _ligands[index][0],
-                                style: const TextStyle(
-                                  fontFamily: '_2',
-                                  fontWeight: FontWeight.bold,
+                              AnimatedPositioned(
+                                duration: const Duration(milliseconds: 200),
+                                left: 0,
+                                right: 0,
+                                bottom: isExpanded ? 0 : -30,
+                                child: Container(
+                                  height: 30,
+                                  decoration: const BoxDecoration(
+                                    color: Colors.blue,
+                                  ),
+                                  child: Center(
+                                    child: InkWell(
+                                      onTap: () => {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                MoleculeScreen(
+                                                    id: _selectedLigand),
+                                          ),
+                                        ).then((value) {
+                                          setState(() {
+                                            _selectedLigand = '';
+                                          });
+                                        }),
+                                      },
+                                      child: const Text(
+                                        'Show in 3D',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: '_2',
+                                        ),
+                                      ),
+                                    ),
+                                  ),
                                 ),
                               ),
                             ],
